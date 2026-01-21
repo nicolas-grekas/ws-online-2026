4d10042ab188d515b31ba07a52d58de615b449e0
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-01-21 11:35:57 +0100

    MapQueryParam

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,15 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Request $request): Response
+    public function index(
+        #[MapQueryParameter('hello')]
+        string $name = 'World',
+    ): Response
     {
         $greet = '';
-        if ($name = $request->query->get('hello')) {
+        if ($name) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
 
